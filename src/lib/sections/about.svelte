33748a6d91c66dc691579924f4f67a9d90a6664f
SiShdroid <saisushma.er@gmail.com>
2026-02-05 20:50:43 -0600

    Update site content

--- src/lib/sections/about.svelte
+++ src/lib/sections/about.svelte
@@ -12,17 +12,17 @@
     <p
         class="mb-16 pr-24 text-2xl font-medium leading-tight tracking-tight text-slate-300 md:text-3xl lg:w-1/2"
     >
-        My name is <span class="font-bold">Hayden Constas</span>. I'm an electrical engineer with a background in entrepreneurship. I've learned a lot the hard way, and am always looking forward to the next challenge.
+        My name is <span class="font-bold">Sai Sushma Pasupuleti</span>. I'm an electrical engineer PhD student with specialization in Power Electronics. I'm always learning new things and love solving problems. I'm always looking forward to the next challenge.
     </p>
     <div class="gap-24 text-pretty md:columns-2">
         <p class="text-sm leading-normal text-slate-400 sm:text-base">
-            I have experience working on advanced electrical systems, specifically in embedded systems and IoT devices, using various programming languages and frameworks. My work includes the development of firmware, software applications, and hardware integration.
+            I have experience building systems from scratch, usually from theory to implementation. I work on advanced power electronic systems, that includes design and control of power converters, using PCB Sltium software, programming with microcontrollers, embedded systems and MATLAB simulations. Experimental work includes soldering, testing devices (oscilloscopes, multimeter, etc) and hardware integration.
         </p>
         <p class="mt-8 text-sm leading-normal text-slate-400 sm:text-base">
-            In my professional journey, I have also focused on software development and system optimization, utilizing technologies like C#, .NET, Azure, JavaScript, TypeScript, and Svelte. My experience includes developing web applications, mobile apps, and backend services.
+            In my journey, my interest always navigated me towards working on real-world problems using advanced technology such as GaN devices. My focus is on building Solid-State device based power converter system that is robust and targets to improve current technology. Alongside, I also work with improving my skills in software systems such as Altium PCB Designer, MATLAB Simulink, LTSpice and programming languages such as C, Python, and DSP microcontroller programming.
         </p>
         <p class="mt-8 text-sm leading-normal text-slate-400 sm:text-base">
-            Recently, I completed a Bachelor of Science in Electrical Engineering, where I gained valuable hands-on experience in both hardware and software development. I've worked on various projects including embedded systems, web applications, and cloud services.
+            I am currently pursuing PhD in Power Electronics at University of Houston, Tx.  valuable hands-on experience in both hardware and software development. I've worked on various projects including embedded systems, web applications, and cloud services.
         </p>
         <p class="mt-8 text-sm leading-normal text-slate-400 sm:text-base">
             Feel free to keep scrolling to learn more about my work and achievements. For additional details or references, please contact me directly.
