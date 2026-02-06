42b81b54d084ff4a1976e7fe7bacd29c0aaa1806
SiShdroid <saisushma.er@gmail.com>
2026-02-06 15:30:31 -0600

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({ fallback: '404.html' })
+    adapter: adapter({ fallback: '404.html' }),
+    prerender: {
+      handleHttpError: ({ status, path, referrer }) => {
+        if (status === 404) return; // ignore missing assets/links
+        throw new Error(`${status} ${path} (linked from ${referrer})`);
+      }
+    }
   }
 };
 
